76e24527057251ba837972668e62d8da2dcc69d8
UronShrestha <150113831+UronShrestha@users.noreply.github.com>
2024-03-30 15:51:07 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,187 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>"Choose us for your gadget needs because we offer a curated selection of cutting-edge technology, unbeatable prices, and exceptional customer service. Our commitment to quality, timely deliveries, and hassle-free returns ensures a seamless shopping experience. Join us in embracing the future of technology shopping."</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's reviews</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-1.png" alt="">
+         <p>'Exceptional service and an incredible range of gadgets!' </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Ben</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-2.png" alt="">
+         <p>'Fast shipping and unbeatable prices. Highly recommended!'</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Gwen</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-3.png" alt="">
+         <p>'I found my dream gadget here. Thanks, guys!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Kevin</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-4.png" alt="">
+         <p>'Effortless returns and top-notch customer support.'</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Liz</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.png" alt="">
+         <p>"Five-star service from start to finish. The quality of the products and the convenience of shopping here keep me coming back." </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Max</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-6.png" alt="">
+         <p>"Impressed by the knowledgeable staff who helped me choose the perfect gadget. A great shopping experience!"</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Mary</h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
